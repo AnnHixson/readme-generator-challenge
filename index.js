e0eca7350d67ab2c5bb306bccd491d67d1fb5953
Ann <ann.hixson@uconn.edu>
2022-11-09 15:54:25 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = ["What is the title of your project?", "Describe your project:", "How does the user install your application?", "What is the usage information for your application?", "Who contributed to your project?", "What are the test instructions for your application?", "Which of these licenses does your application have?", "What is your GitHub username?", "What is your email address?"];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
